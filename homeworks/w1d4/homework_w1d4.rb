ef5414278dac0c5994d1c0778e0e0c71a5e895dd
Mark Noizumi <Mark Noizumi>
2016-11-17 23:13:33 -0800

    Create Map class

--- homeworks/w1d4/homework_w1d4.rb
+++ homeworks/w1d4/homework_w1d4.rb
@@ -47,11 +47,55 @@ class Queue
 
 end
 
-q = Queue.new
-q.enqueue(2)
-q.enqueue(3)
-q.enqueue(4)
-q.show
-p q.dequeue
-p q.dequeue
-q.show 
+# q = Queue.new
+# q.enqueue(2)
+# q.enqueue(3)
+# q.enqueue(4)
+# q.show
+# p q.dequeue
+# p q.dequeue
+# q.show
+class Map
+ attr_accessor :map
+
+ def initialize
+   @map = []
+ end
+
+ def assign(key, value)
+  flag = true
+  map.each_with_index do |el, i|
+    if el[0] == key
+      map[i][1] = value
+      flag = false
+    end
+  end
+  @map << [key, value] if flag
+ end
+
+ def lookup(key)
+   map.each_with_index do |el, i|
+     return el[1] if el[0] == key
+   end
+ end
+
+ def remove(key)
+   map.each_with_index do |el, i|
+     map.delete(el) if el[0] == key
+   end
+ end
+
+ def show
+   p @map
+ end
+
+end
+# m = Map.new
+# m.assign("dog", true)
+# m.assign("cat", true)
+# m.show
+# m.assign("dog", false)
+# m.show
+# p m.lookup("dog")
+# m.remove("cat")
+# p m.show
